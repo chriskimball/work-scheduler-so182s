ee3f68ce11d97fb9376d9f7422fc7aea44a61657
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-12-04 01:05:25 -0800

    Commented the JS file, moved init function to bottom of page

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 
-// select and declare variables for the following elements
-// so we can loop and append these elements
-
-//  $('.container') to render and print html in the container
-
-//  $('#currentDay') to render and print html in the container
-//  currentHour
-
-// Day of week variable
+// Current day of week variable
 var dayOfWeek = moment().format('dddd, MMMM Do');
 
-// hour of day variable
+// Current hour of day variable
 var timeOfDay = moment().format('H')
-// var timeOfDay = 11
 
-console.log(timeOfDay)
+// Appending current day of week + date into current day element
 $('#currentDay').text(dayOfWeek);
 
+// Declaring variable for the container element to print hour blocks
 var ContainerEl = $(".container")
 
+// Initial page load function
 function init(){
-    // for loop from 9 to 17
+    // For hours 9 to 17 (9am to 5pm) do the following
     for (var i = 9; i <= 17; i++){
-        // retreiving hour text
+        // Retrieve hour text from local storage
         var hourElText = JSON.parse(localStorage.getItem("hour-"+i));
         
         // If local storage is blank then it will set the text area to a blank string
         if (hourElText === null) {
             hourElText = "";
         }
-        
+
+        // Adding hour styling for past/current/future timeblocks
         var hourStyle = ""
-        // Adding style elements for past/current/future timeblocks
         if (timeOfDay == i) {
             hourStyle = " present";
         } else if (timeOfDay < i) {
@@ -41,7 +34,7 @@ function init(){
             hourStyle = " past";
         } 
 
-        // create new block of html for each hour of the day 
+        // create new block of html for each hour of the work day 
         var htmlTemplate = `
         <div id="hour-${i}" class="row">
             <label class="hour col-1 p-3 text-right" for="hour">${moment(i, 'HH').format('hA')}</label>
@@ -50,47 +43,24 @@ function init(){
         </div>
         `
         
+        // Appending htmlTemplate into container element
         ContainerEl.append(htmlTemplate)
     }
 }
 
-init()
-
-    // saving the current hour to a data attribute so it can be accessed from an event listener
-    
-    // i = current hour of the loop, 9 - 17
-
-    // fetch saved values in for loop
-    // local storage key = "hour-9"
-    // need to put hour- i variable into a string
-    // var savedValueForHour = localStorage.getItem( 'hour-' + i ,  )
-
-    // need to do something similar when we save the items to convert a saved number value into a string to put into local storage.
-    
-    // data attributes are key
-    
-    
-
-
-// add event listener to container element so we can create dynamic buttons that are rendered on the page (children of the container and event delegation)
-// event listener on the container can listen to any button click
-
-// need to capture the container element into a variable
+// Event listener on the container can listen to any button click
 $('.container').on('click','button', function(event){
     
-    // we need to respond to button clicks and interact with data. Access a data attribute from button or parent element.
-    // need to add 'event' object as a callback argument
     event.preventDefault()
-    // access the current target
+
+    // Access the current target's data attribute for hour of day
     var hourClicked = event.target.dataset.hour
+    // Selecting the textarea value for the button that was clicked based on the button's data attribute
     var hourEl = $('#hour-' + hourClicked).children().eq(1).val()
-
+    
+    // Saves textarea value into the local storage
     localStorage.setItem("hour-" + hourClicked, JSON.stringify(hourEl));
-    // var hourTextEl = hourEl.children
-    console.log(hourEl)
-    
-    // access the targets data attributes
-    // event.target.dataset. aka vanilla JS might be better than jQuery .data method to access the data attributes
-    
-
 })
+
+// Calling initial page load function
+init()
